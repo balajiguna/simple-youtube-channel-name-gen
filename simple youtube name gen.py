392640d9ee67253bed4c5bea585c94dbba8a85eb
BALAJI GUNA <58567028+balajiguna@users.noreply.github.com>
2021-07-01 11:20:50 +0530

    Add files via upload

---
+++ simple youtube name gen.py
@@ -0,0 +1,8 @@
+#Greeting for your Simple Youtube Channel Name Generator!
+print("Welcome to the Simple Youtube Channel Name Generator.\n")
+#ask for users favourite animal name.
+animal = input("what is your favourite animal ?\n")
+#ask for users hobby.
+hobby = input("what is your hobby?\n")
+# Combine the favourite animal name and hobby and show them their Youtube Channel Name.
+print("Your Youtube Channel name could be " + animal + " " + hobby)
